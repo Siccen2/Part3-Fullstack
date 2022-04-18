b3e429baa6fe6121f5df2331dece9ec385aa777c
Siccen2 <martin.harmala@centria.fi>
2022-04-18 13:39:34 +0300

    part3.6

--- README.md
+++ README.md
@@ -2,4 +2,3 @@
 Part 3 of Fullstack course.
 
 
-Donat start from Deliting recorses
--- index.js
+++ index.js
@@ -1,50 +1,104 @@
 const express = require('express')
 const app = express()
+app.use(express.json())
 
-let notes = [
-    {
-        id: 1,
-        content: "HTML is easy",
-        date: "2022-05-30T17:30:31.098Z",
-        important: true
-      },
-      {
-        id: 2,
-        content: "Browser can execute only Javascript",
-        date: "2022-05-30T18:39:34.091Z",
-        important: false
-      },
-      {
-        id: 3,
-        content: "GET and POST are the most important methods of HTTP protocol",
-        date: "2022-05-30T19:20:14.298Z",
-        important: true
-      }
+let persons = [
+  { 
+    "id": 1,
+    "name": "Arto Hellas", 
+    "number": "040-123456"
+  },
+  { 
+    "id": 2,
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523"
+  },
+  { 
+    "id": 3,
+    "name": "Dan Abramov", 
+    "number": "12-43-234345"
+  },
+  { 
+    "id": 4,
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122"
+  }
 ]
+
+const generateId = () => {
+  const maxId = persons.length > 0
+    ? Math.max(...persons.map(n => n.id))
+    : 0
+  return maxId + 1
+}
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+  
+  //Check if names are the same
+    if (persons.find(person => person.name === body.name)) {
+    return response.status(400).json({ 
+      error: 'name already exist' 
+    })
+  }
+  //name misssing
+  if (!body.name) {
+    return response.status(400).json({ 
+      error: 'name missing' 
+    })
+  }
+  //number missing
+  if (!body.number) {
+    return response.status(400).json({ 
+      error: 'number missing' 
+    })
+  }
+
+  const person = {
+    id: generateId(),
+    name: body.name,
+    number: body.number,
+    
+  }
+
+  persons = persons.concat(person)
+
+  response.json(person)
+})
+
 
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
 
-app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const note = notes.find(note => note.id === id)
-    
-    if (note) {
-      response.json(note)
-    } else {
-      response.status(404).end()
-    }
-  })
-  
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const note = persons.find(note => note.id === id)
 
-
-app.get('/api/notes', (request, response) => {
-  response.json(notes)
+  if (note) {
+    response.json(note)
+  } else {
+    response.status(404).end()
+  }
 })
 
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(note => note.id !== id)
 
+  response.status(204).end()
+})
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+app.get('/infos', (request, response) => {
+  const length = persons.length
+  const date = new Date()
+  response.send(`<p>Phonebook has info for ${length} people</p><p>${date}</p>`)
+})
 
 const PORT = 3001
 app.listen(PORT, () => {
