9bc23a756de43406ba5de1e7355e0d4b9becb575
Siccen2 <martin.harmala@centria.fi>
2022-04-20 13:42:49 +0300

    heroko2

--- index.js
+++ index.js
@@ -109,6 +109,7 @@ app.get('/infos', (request, response) => {
   response.send(`<p>Phonebook has info for ${length} people</p><p>${date}</p>`)
 })
 
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
